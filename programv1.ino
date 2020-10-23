86016e161896d5cf4c30b9d3f8e32f86c89d7489
Jonathan Parhusip <parhusipjonathan@gmail.com>
2020-10-23 13:52:49 +0700

    program multi relay

---
+++ programv1.ino
@@ -0,0 +1,58 @@
+//deklarasi firebase
+#include <WiFi.h>
+#include <FirebaseESP32.h>
+#define FIREBASE_HOST "https://iotbox-e3bbd.firebaseio.com/"
+#define FIREBASE_AUTH "T9cz72ToRljGDoF5CpgBrss5yH6m5dflf2CRo9OZ"
+FirebaseData firebaseData;
+unsigned long waktusebelum = 0;
+
+#define lockfront 23
+#define lockback 22
+#define sinaruv 21
+
+#define sensor_pintu 19
+#define sensor_sharp A3
+#define sensor_uv A0
+#define uvlight 13
+int state = 0;
+int barangbersih = 0;
+
+//dht22
+#include "DHT.h"
+#define DHTPIN 14
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(sensor_pintu, INPUT_PULLUP);
+  pinMode(2, OUTPUT);
+  pinMode(sinaruv, OUTPUT);
+  pinMode(lockfront, OUTPUT);
+  pinMode(lockback, OUTPUT);
+
+  digitalWrite(lockfront, 1);
+  digitalWrite(lockback, 1);
+  digitalWrite(sinaruv, 1);
+
+  //inisiasi firebase
+  WiFi.disconnect();
+  delay(3000);
+  Serial.println("START");
+  WiFi.begin("HKTI PROVINSI", "tanijaya");
+  while ((!(WiFi.status() == WL_CONNECTED))) {
+    delay(300);
+    Serial.print("..");
+  }
+  Serial.println("Connected");
+  Serial.println("Your IP is");
+  Serial.println((WiFi.localIP()));
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+
+  dht.begin();
+}
+
+void loop() {
+  lock();
+}
